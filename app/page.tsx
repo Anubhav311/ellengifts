a89b598f99207fbdedbd9ca0145c992c88efe658
Anubhav Bhambri <anubhavbhambri1@gmail.com>
2023-08-04 00:41:09 +0530

    created separate route for listing

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@
 
 import { Toaster } from "@/components/ui/toaster";
 import { Upload } from "@/components/Upload";
-import { ImagesList } from "@/components/Images";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Upload />
-      <ImagesList />
       <Toaster />
     </main>
   );
