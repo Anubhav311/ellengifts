983991a3b23369119f9edbbf3368d4b3b7be2066
Anubhav Bhambri <anubhavbhambri1@gmail.com>
2023-08-04 15:35:48 +0530

    added navbar

--- app/dieuOUDjejwksdUOd211duUDDLkkK/page.tsx
+++ app/dieuOUDjejwksdUOd211duUDDLkkK/page.tsx
@@ -6,6 +6,8 @@ import { ref, getDownloadURL, listAll } from "firebase/storage";
 import { storage } from "@/firebase";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { saveAs } from "file-saver";
 
 export interface IImagesListProps {}
 
@@ -36,8 +38,8 @@ export default function ImagesList(props: IImagesListProps) {
               <Image alt="image" src={url} key={url} width={500} height={500} />
             </CardContent>
             <CardFooter className="justify-end">
-              {/* <Button variant="outline">Remove</Button> */}
               <Button onClick={handleDownloa}>Download</Button>
+              <FirebaseImageDownloadButton imagePath={url} />
             </CardFooter>
           </Card>
         );
@@ -45,3 +47,62 @@ export default function ImagesList(props: IImagesListProps) {
     </div>
   );
 }
+
+// interface ImageDownloadButtonProps {
+//   imageUrl: string;
+// }
+
+// const ImageDownloadButton: React.FC<ImageDownloadButtonProps> = ({
+//   imageUrl,
+// }) => {
+//   const handleDownload = async () => {
+//     try {
+//       const response = await axios.get(imageUrl, {
+//         responseType: "blob", // Important for downloading binary files
+//       });
+
+//       const blob = new Blob([response.data], { type: "image/png" }); // Adjust the MIME type accordingly
+
+//       // Extract the image file name from the URL (you may need to adjust this based on your URL structure)
+//       const urlParts = imageUrl.split("/");
+//       const fileName = urlParts[urlParts.length - 1];
+
+//       saveAs(blob, fileName);
+//     } catch (error) {
+//       console.error("Error while downloading the image:", error);
+//     }
+//   };
+
+//   return <button onClick={handleDownload}>Download Image</button>;
+// };
+
+// import firebase from 'firebase/app';
+// import 'firebase/storage';
+
+interface FirebaseImageDownloadButtonProps {
+  imagePath: string;
+}
+
+const FirebaseImageDownloadButton: React.FC<
+  FirebaseImageDownloadButtonProps
+> = ({ imagePath }) => {
+  const handleDownload = async () => {
+    try {
+      const storageRef = ref(storage, imagePath);
+      const imageFile = await getDownloadURL(storageRef);
+
+      // Fetch the image data
+      const response = await fetch(imageFile);
+      const blob = await response.blob();
+
+      // Extract the image file name from the path (you may need to adjust this based on your path structure)
+      const fileName = imagePath.split("/").pop();
+
+      saveAs(blob, fileName || "downloaded_image.png");
+    } catch (error) {
+      console.error("Error while downloading the image:", error);
+    }
+  };
+
+  return <button onClick={handleDownload}>Download Image</button>;
+};
